140489a40d502bea40ad8d3791fd52861deb2c50
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-12-19 03:51:47 +0530

    Changes UI & UX and Responsive added useg media queary

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+//STICKY NAV
+let header=document.querySelector(".header")
+window.addEventListener("scroll",()=>{
+  if(window.scrollY>120){
+    header.classList.add("sticky")
+  }
+  else {
+    header.classList.remove("sticky");
+}
+})
+
+
+
+//mobile nav bar code
+let menu=document.querySelector("#menu-icon")
+let navList=document.querySelector(".nav-list")
+menu.onclick=()=>{
+  menu.classList.toggle("bx-x") //bx-x is close outline class name dynamicaly added class naame
+  navList.classList.toggle("active")
+}
+
+window.onscroll=()=>{
+  menu.classList.remove("bx-x")
+  navList.classList.remove("active")
+}
+
+
+//resume download function
+function downloadResume() {
+  let pdfUrl = 'CV_2023070500361786.pdf';
+  let link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'Toufik_Resume.pdf';
+  link.target = '_blank';
+  link.click();
+}
